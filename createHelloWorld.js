2254464adeb18c8c5e6614b1f3085076562df827
alivia-23 <guin.al@husky.neu.edu>
2024-09-06 14:21:05 -0700

    closure problem

---
+++ createHelloWorld.js
@@ -0,0 +1,10 @@
+// Example of Closure where the inner function is returned from the outer function
+
+var createHelloWorld = function() {
+    return function(...args) {
+        return 'Hello World';
+    }
+}
+
+const f = createHelloWorld();
+console.log(f());
